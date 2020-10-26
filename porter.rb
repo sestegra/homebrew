e9709e34c01d8a8e8f13f2f9ec6bda6e06debdb9
Stéphane Este-Gracias <sestegra@gmail.com>
2020-10-26 19:20:44 +0100

    Update porter.rb

--- porter.rb
+++ porter.rb
@@ -3,7 +3,7 @@ class Porter < Formula
   homepage "https://porter.sh"
   license "Apache-2.0"
 
-  version "v0.29.0"
+  version "0.29.0"
   if OS.mac?
     url "https://cdn.porter.sh/v0.29.0/porter-darwin-amd64"
     sha256 "b200be09146d6ca6b6f277e01e0b98cf9464b6cd343a2234db4c981a230c6db3"
