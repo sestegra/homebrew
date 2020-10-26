28fbd3c272bc69ebf6f4fa2f953da53028b7f7aa
Stéphane Este-Gracias <sestegra@gmail.com>
2020-10-26 19:38:34 +0100

    Revert to 0.29.0 without Linux
    Signed-off-by: Stéphane Este-Gracias <sestegra@gmail.com>

--- porter.rb
+++ porter.rb
@@ -2,20 +2,12 @@ class Porter < Formula
   desc "Porter"
   homepage "https://porter.sh"
   license "Apache-2.0"
-
-  version "0.29.1"
-  if OS.mac?
-    url "https://cdn.porter.sh/v0.29.1/porter-darwin-amd64"
-    sha256 "b200be09146d6ca6b6f277e01e0b98cf9464b6cd343a2234db4c981a230c6db3"
-    $binary_name = "porter-darwin-amd64"
-  elsif OS.linux?
-    url "https://cdn.porter.sh/v0.29.1/porter-linux-amd64"
-    sha256 "402e3e6d130b297edbfed3f41f938eee8cedbd63d906cef88710940054831b4f"
-    $binary_name = "porter-linux-amd64"
-  end
+  version "0.29.0"
+  url "https://cdn.porter.sh/v0.29.0/porter-darwin-amd64"
+  sha256 "b200be09146d6ca6b6f277e01e0b98cf9464b6cd343a2234db4c981a230c6db3"
 
   def install
-    system "mv", $binary_name, "porter"
+    system "mv", "porter-darwin-amd64", "porter"
     bin.install "porter"
   end
 
