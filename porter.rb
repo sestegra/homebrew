122f30f27eb01fd5a62e40d9f5a7b30f1184e896
Stéphane Este-Gracias <sestegra@gmail.com>
2021-06-26 11:57:22 +0200

    Update to v0.38.3
    Signed-off-by: Stéphane Este-Gracias <sestegra@gmail.com>

--- porter.rb
+++ porter.rb
@@ -2,13 +2,25 @@ class Porter < Formula
   desc "Porter"
   homepage "https://porter.sh"
   license "Apache-2.0"
-  version "0.29.1"
-  url "https://cdn.porter.sh/v0.29.1/porter-darwin-amd64"
-  sha256 "64d8c5a1378465d9e304531a06649d911575f872c385d0986df7f2ef26dc6896"
+  version "0.38.3"
+
+  if OS.mac?
+    url "https://github.com/getporter/porter/releases/download/v0.38.3/porter-darwin-amd64"
+    sha256 "5c09344a1349c4c2469dc606c4e785d85ebd00a3723e228891e300fe181db828"
+  end
+
+  if OS.linux?
+    url "https://github.com/getporter/porter/releases/download/v0.38.3/porter-linux-amd64"
+    sha256 "4fcd59dcf972db5e64b44f13980146700ea2e348761a379a0214a3f6aa6a3ad2"
+  end
 
   def install
-    system "mv", "porter-darwin-amd64", "porter"
-    bin.install "porter"
+    if OS.mac?
+      bin.install "porter-darwin-amd64" => "porter"
+    end
+    if OS.linux?
+      bin.install "porter-linux-amd64" => "porter"
+    end
   end
 
   test do
